d67a304aaaec302b05707fe5aaff2eea0a2b0230
Khorrlybobo <koladeemmanuela32@gmail.com>
2020-07-14 03:06:49 +0100

    it is done

--- CoursesWebAapps/src/main/java/com/example/demo/CoursesController.java
+++ CoursesWebAapps/src/main/java/com/example/demo/CoursesController.java
@@ -4,17 +4,16 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
 @Controller
 public class CoursesController {
-	@RequestMapping("courses")
-	@ResponseBody
-	public String courss() {
+	@RequestMapping("/courses")
+
+	public String courses() {
 		System.out.println("Welcome to edureka");
-		return "course";
+		return "course.jsp";
 
 	}
 
